28a7df34b6e2f1fc5611aa0e91233e2e553cc030
Travis Fischer <fisch0920@gmail.com>
2021-03-21 14:43:14 -0400

    🚣

--- clubhouse-client.js
+++ clubhouse-client.js
@@ -11,44 +11,29 @@ export class ClubhouseClient {
   _userId = null
   _token = null
 
-  constructor(opts) {
-    if (!opts) throw new Error('Missing required ClubhouseClient opts')
-
+  constructor(opts = {}) {
     const {
       deviceId,
       userId,
       token,
+      apiBaseUrl,
+      appBuild = '304',
+      appVersion = '0.1.28',
       throttle = {
         limit: 1,
         interval: 2000
       }
     } = opts
 
-    if (!deviceId) {
-      throw new Error('Missing required ClubhouseClient opts.deviceId')
-    }
-
-    if (!userId) {
-      throw new Error('Missing required ClubhouseClient opts.userId')
-    }
-
-    if (!token) {
-      throw new Error('Missing required ClubhouseClient opts.token')
-    }
-
-    this._apiBaseUrl = opts.apiBaseUrl || 'https://www.clubhouseapi.com/api'
-    //  || 'https://api.hipster.house/api'
+    this._apiBaseUrl = apiBaseUrl || 'https://www.clubhouseapi.com/api'
+    // 'https://api.hipster.house/api'
 
     this._deviceId = deviceId
     this._userId = userId
     this._token = token
 
+    // throttle clubhouse API calls to mitigate rate limiting
     this._fetch = pThrottle(throttle)(this.__fetch)
-
-    // const appBuild = '269'
-    // const appVersion = '0.1.15'
-    const appBuild = '304'
-    const appVersion = '0.1.28'
 
     this._headers = {
       'ch-appbuild': appBuild,
@@ -58,6 +43,40 @@ export class ClubhouseClient {
       'user-agent': `clubhouse/${appVersion} (iPhone; iOS 13.5.1; Scale/3.00)`,
       ...opts.headers
     }
+  }
+
+  startPhoneNumberAuth(phoneNumber) {
+    return this._fetch({
+      auth: false,
+      endpoint: `/start_phone_number_auth`,
+      method: 'POST',
+      body: {
+        phoneNumber
+      }
+    })
+  }
+
+  resendPhoneNumberAuth(phoneNumber) {
+    return this._fetch({
+      auth: false,
+      endpoint: `/resend_phone_number_auth`,
+      method: 'POST',
+      body: {
+        phoneNumber
+      }
+    })
+  }
+
+  completePhoneNumberAuth(phoneNumber, verificationCode) {
+    return this._fetch({
+      auth: false,
+      endpoint: `/complete_phone_number_auth`,
+      method: 'POST',
+      body: {
+        phoneNumber,
+        verificationCode
+      }
+    })
   }
 
   getProfile(userId) {
@@ -164,17 +183,40 @@ export class ClubhouseClient {
     return users
   }
 
-  __fetch({ endpoint, method = 'GET', body, gotOptions, headers, ...rest }) {
+  get isAuthenticated() {
+    return this._token && this._deviceId && this._userId
+  }
+
+  __fetch({
+    endpoint,
+    method = 'GET',
+    auth = true,
+    body,
+    gotOptions,
+    headers,
+    ...rest
+  }) {
     const apiUrl = `${this._apiBaseUrl}${endpoint}`
+
+    let authHeaders
+    if (auth) {
+      if (!this.isAuthenticated) {
+        throw new Error(`Error ${endpoint} requires authentication`)
+      }
+
+      authHeaders = {
+        authorization: `Token ${this._token}`,
+        'ch-deviceid': this._deviceId,
+        'ch-userid': this._userId
+      }
+    }
 
     const params = {
       method,
       ...gotOptions,
       headers: {
         ...this._headers,
-        authorization: `Token ${this._token}`,
-        'ch-deviceid': this._deviceId,
-        'ch-userid': this._userId,
+        ...authHeaders,
         ...headers
       },
       ...rest
